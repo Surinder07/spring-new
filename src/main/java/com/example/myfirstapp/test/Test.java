4436642035a91e2970a82b1e221d4ec9be608f4e
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-09-09 22:58:25 -0400

    added springboot actuator

---
+++ src/main/java/com/example/myfirstapp/test/Test.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "hello world";
+    }
+
+
+
+
+
+
+
+
+}
